3e7e857c8e8b24677c75442d3d960e461d06e599
weter123 <algailani.ahmad@gmail.com>
2025-01-11 23:29:06 -0500

    First commit

---
+++ Excel_to_SQL.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import sqlite3
+
+exit = False
+
+while exit != True:
+
+    try:
+        sql_file = input("Enter SQl filename:")
+        excel_file = input("Enter Excel Filename:")
+
+        conn = sqlite3.connect(f'{sql_file}.db')
+
+        excel_df = pd.ExcelFile(f'{excel_file}')
+        sheets_num = len(excel_df.sheet_names)
+        print(sheets_num)
+        sheet =0
+
+        while sheet < sheets_num:
+            sheet_name = excel_df.sheet_names[sheet]
+            df = excel_df.parse(sheet_name)
+            df.to_sql(sheet_name,conn,if_exists='replace',index=False)
+            df.to_csv(f'{sheet_name}.csv', index=False)
+            sheet = sheet + 1
+        
+    except Exception as error:
+        print("Error has occured", error)
+    else:
+        exit = True
+print("done!")
+
+
+
+
+
---
+++ csv_to_SQL.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import sqlite3
+
+
+exit = False
+
+while exit != True:
+    try:
+        sql_file = input("Enter SQl filename:")
+        csv_file = input("Enter csv filename:")
+
+        conn = sqlite3.connect(f'{sql_file}.db')
+
+        csv_df = pd.read_csv(f'{csv_file}.csv')
+
+
+        csv_df.to_sql(csv_file,conn,if_exists='replace',index=False)
+
+    except Exception as error:
+        print("Error has occured", error)
+    else:
+        exit = True
+print("done!")
+
+
+
